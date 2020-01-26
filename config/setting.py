6d939b54060085465071e10e04d3df1995707102
jesseslco <jesseslco@gmail.com>
2020-01-26 22:54:32 +0800

    adding dev branch

--- config/setting.py
+++ config/setting.py
@@ -1,8 +1,8 @@
 import os
 
-proxy = None
-# proxy = {"http":"http://127.0.0.1:7890",
-#          "https":"http://127.0.0.1:7890"}
+# proxy = None
+proxy = {"http":"http://127.0.0.1:7890",
+         "https":"http://127.0.0.1:7890"}
 
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36',
            'method': "GET"}
